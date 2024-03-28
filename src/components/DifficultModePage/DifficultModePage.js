90309c0a324d45b697d75c81bbf8ede22ac5010c
Jen Nguyen <jenniferannnguyen@live.com>
2024-03-28 11:40:19 -0600

    Add responsiveness to difficut mode

--- src/components/DifficultModePage/DifficultModePage.js
+++ src/components/DifficultModePage/DifficultModePage.js
@@ -14,47 +14,42 @@ const DifficultModePage = ({
 }) => {
   const [playerChoice, setPlayerChoice] = useState(null);
   const [cpuChoice, setCpuChoice] = useState(null);
+  const [result, setResult] = useState(null);
 
   const options = [
-    { name: 'MasterShell', image: MasterShellImage },
-    { name: 'BananaPeel', image: BananaPeelImage },
-    { name: 'Lightning', image: LightningImage },
-    { name: 'Mushroom', image: MushroomImage },
-    { name: 'Bullet', image: BulletImage },
+    { image: MasterShellImage, name: 'MasterShell' },
+    { image: BananaPeelImage, name: 'BananaPeel' },
+    { image: LightningImage, name: 'Lightning' },
+    { image: MushroomImage, name: 'Mushroom' },
+    { image: BulletImage, name: 'Bullet' },
   ];
 
   const handlePlayerChoice = index => {
     const randomIndex = Math.floor(Math.random() * options.length);
-    // const cpuChoice = randomIndex;
-    setPlayerChoice(options[index]);
-    setCpuChoice(options[randomIndex]);
-    const result = determineWinner(options[index], options[randomIndex]);
-    if (result === 'You win!') {
+    const cpuIndex = randomIndex;
+    const winner = determineWinner(index, cpuIndex);
+
+    if (winner === 'You win!') {
       setPlayerScore(playerScore + 1);
-    } else if (result === 'CPU wins!') {
+    } else if (winner === 'CPU wins!') {
       setCpuScore(cpuScore + 1);
     }
+
+    setResult(winner);
+    setPlayerChoice(options[index]);
+    setCpuChoice(options[cpuIndex]);
   };
 
-  const determineWinner = (playerChoice, cpuChoice) => {
-    if (playerChoice === null || cpuChoice === null) {
-      return '';
-    }
-
-    const rules = {
-      MasterShell: ['Lightning', 'Mushroom'],
-      BananaPeel: ['MasterShell', 'Bullet'],
-      Lightning: ['BananaPeel', 'Mushroom'],
-      Mushroom: ['BananaPeel', 'Bullet'],
-      Bullet: ['Lightning', 'MasterShell'],
-    };
-
-    const playerChoiceName = playerChoice.name;
-    const cpuChoiceName = cpuChoice.name;
-
-    if (playerChoiceName === cpuChoiceName) {
+  const determineWinner = (playerIndex, cpuIndex) => {
+    if (playerIndex === cpuIndex) {
       return "It's a tie!";
-    } else if (rules[playerChoiceName].includes(cpuChoiceName)) {
+    } else if (
+      (playerIndex === 0 && cpuIndex === 2) ||
+      (playerIndex === 1 && cpuIndex === 0) ||
+      (playerIndex === 2 && cpuIndex === 1) ||
+      (playerIndex === 3 && (cpuIndex === 1 || cpuIndex === 4)) ||
+      (playerIndex === 4 && (cpuIndex === 0 || cpuIndex === 2))
+    ) {
       return 'You win!';
     } else {
       return 'CPU wins!';
@@ -62,11 +57,9 @@ const DifficultModePage = ({
   };
 
   const resetScores = () => {
-    setPlayerScore(0)
-    setCpuScore(0)
+    setPlayerScore(0);
+    setCpuScore(0);
   };
-
-  const result = determineWinner(playerChoice, cpuChoice);
 
   return (
     <div className='flex flex-col items-center'>
@@ -78,44 +71,41 @@ const DifficultModePage = ({
               src={option.image}
               alt={option.name}
               onClick={() => handlePlayerChoice(index)}
-              className='w-40 h-auto mr-4 cursor-pointer mx-20'
+              className='w-20 sm:w-40 mr-4 cursor-pointer mx-12 sm:mx-24'
             />
           ))}
         </div>
       </div>
-      <div className='mt-4 flex justify-around w-full'>
-        {playerChoice !== null && (
+      {playerChoice && cpuChoice && (
+        <div className='mt-4 flex justify-around w-full'>
           <div className='bg-gray-700 opacity-90 p-4 rounded-lg flex items-center relative z-10'>
             <h3 className='mr-2 text-white text-3xl'>Player's Choice:</h3>
             <div className='flex items-center'>
               <img
                 src={playerChoice.image}
                 alt={playerChoice.name}
-                className='w-24 h-auto mr-2'
+                className='w-20 sm:w-24 h-auto mr-2'
               />
             </div>
           </div>
-        )}
-        {cpuChoice !== null && (
           <div className='bg-gray-700 opacity-90 p-4 rounded-lg flex items-center relative z-10'>
             <h3 className='mr-2 text-white text-3xl'>CPU's Choice:</h3>
-            <div className='flex items-center'>
+            <div className='flex items-center ml-4 sm:ml-8'> {/* Adjusted margin for smaller screens */}
               <img
                 src={cpuChoice.image}
                 alt={cpuChoice.name}
-                className='w-24 h-auto mr-2'
+                className='w-20 sm:w-24 h-auto mr-2'
               />
             </div>
           </div>
-        )}
-      </div>
-
-      <h1 className='mt-4 text-white relative z-10 text-6xl'>{result}</h1>
+        </div>
+      )}
+      {result && <h1 className='mb-10 text-6xl text-white relative z-10'>{result}</h1>}
       <div className='mt-4 flex justify-center'>
-        <div className='w-100'>
+        <div className='w-full sm:w-auto'>
           <div className='bg-gray-800 opacity-90 p-4 rounded-lg mt-4'>
-            <h2 className='text-4xl font-bold mb-2 text-white'>Game Rules:</h2>
-            <ul className='list-none ml-4 text-white text-4xl'>
+            <h2 className='text-3xl sm:text-4xl font-bold mb-2 text-white text-center'>Game Rules:</h2>
+            <ul className='list-none ml-4 text-white text-2xl sm:text-3xl'>
               <li>MasterShell &gt; Lightning</li>
               <li>BananaPeel &gt; MasterShell</li>
               <li>Lightning &gt; BananaPeel</li>
@@ -126,14 +116,14 @@ const DifficultModePage = ({
         </div>
       </div>
       <div className='mt-4 flex justify-center'>
-      <button
+        <button
           onClick={resetScores}
-          className='bg-gray-700 hover:bg-gray-500 opacity-90 text-white text-3xl font-bold py-2 px-4 rounded mr-4 relative z-10'>
+          className='bg-gray-700 hover:bg-gray-500 opacity-90 text-white text-xl sm:text-3xl font-bold py-2 px-4 rounded mr-4 relative z-10'>
           Reset Scores
         </button>
         <Link
           to='/'
-          className='bg-gray-700 hover:bg-gray-500 text-white text-3xl font-bold py-2 px-4 rounded mr-4 relative z-10'>
+          className='bg-gray-700 hover:bg-gray-500 text-white text-xl sm:text-3xl font-bold py-2 px-4 rounded mr-4 relative z-10'>
           Back to Main
         </Link>
       </div>
