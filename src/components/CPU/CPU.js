a1be8bd43c244bf03bc47b7be23525a02de66519
Jen Nguyen <jenniferannnguyen@live.com>
2024-04-02 10:43:54 -0600

    Make player icons smaller on mobile screens

--- src/components/CPU/CPU.js
+++ src/components/CPU/CPU.js
@@ -4,7 +4,7 @@ const CPU = ({ cpuScore }) => {
   return (
     <main>
       <div className='bg-gray-800 bg-opacity-90 rounded-lg p-2 sm:p-4 md:p-6 w-24 sm:w-32 md:w-48 mx-auto text-center relative z-10 mr-4'>
-        <img src={KoopaTroopa} alt='koopa troopa' className='w-full h-auto' />
+        <img src={KoopaTroopa} alt='koopa troopa' className='w-full h-auto sm:w-3/4 md:w-full' />
         <p className='text-white text-sm sm:text-xl md:text-3xl'>
           Score: {cpuScore}
         </p>
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -4,7 +4,7 @@ const Player = ({ playerScore }) => {
   return (
     <main>
       <div className='bg-gray-800 bg-opacity-90 rounded-lg p-2 sm:p-4 md:p-6 w-24 sm:w-32 md:w-48 mx-auto text-center relative z-10 ml-4'>
-        <img src={Koopa} alt='koopa' className='w-full h-auto' />
+        <img src={Koopa} alt='koopa' className='w-full h-auto sm:w-3/4 md:w-full' />
         <p className='text-white text-sm sm:text-xl md:text-3xl'>
           Score: {playerScore}
         </p>
